5d3654684166e101b3304361aa4f0e41ac3ef78b
Youmna Elgzery <130307273+youmnaabdelhamid1@users.noreply.github.com>
2025-02-08 16:26:57 +0200

    Task1 H7.2 main.cpp

---
+++ Task1/H7.2 main.cpp
@@ -0,0 +1,30 @@
+//Task1.H7.2
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b,c,temp;
+    cin>> a >> b >> c;
+    if (a>b)
+    {
+        temp= a;
+        a=b;
+        b= temp;
+    }
+    if(a>c)
+    {
+        temp = a;
+        a=c;
+        c=temp;
+    }
+    if(b>c)
+    {
+        temp = b;
+        b=c;
+        c= temp;
+    }
+    cout << a <<" " << b << " " <<c << endl;
+    return 0;
+}
