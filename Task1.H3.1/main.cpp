4b2bcac5a3bd91db3024330877931878997a0919
Youmna Elgzery <130307273+youmnaabdelhamid1@users.noreply.github.com>
2025-02-08 13:21:04 +0200

    Task1.H3.1 main.cpp

---
+++ Task1.H3.1/main.cpp
@@ -0,0 +1,21 @@
+//Task1.H3.1
+#include<iostream>    //<iostream> not <stream
+using namespace std;  // std; not STD
+
+int main() { // main not maIN
+
+    cout <<"work smart not hard\n"; // should not be ouside main
+	cout << "Children must be taught how to think, not what to think\n"; // semicolon
+	cout << "We worry about what a child will become \"tomorrow\" yet we forget that he is someone today\n"; //\"tomorrow\" not "tomorrow"
+	cout << "Children are not things to be molded"<<", but are people to be unfolded\n"; // << not <
+	cout << "Each day of our lives we make deposits in the memory banks of our children."<<endl; // endl not end
+	cout      << "It is easier to build strong children than to repair broken men"<<"\n"; // "" not """"
+	cout << "Children need models rather than critics\n"; // << not >>
+
+	cout <<"Children have never been very good at listening to their elders, but they have never failed to imitate them"; // cout not out
+
+	cout << "Children are our most valuable resource\n"; // << not <
+
+    return 0; // add return 0
+} // } to end main
+
