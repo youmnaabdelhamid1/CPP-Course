087fc1576151148467c47ab8562437cb2d7e471e
Youmna Elgzery <130307273+youmnaabdelhamid1@users.noreply.github.com>
2025-02-08 13:13:34 +0200

    Task1.H2.2 main.cpp

---
+++ Task1.H2.2/main.cpp
@@ -0,0 +1,26 @@
+// هرم من *
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int rows;
+    cout<<"Enter number of rows: "<<endl;
+    cin>> rows;
+    for(int i =1; i<=rows ; i++)
+    {
+        //spaces
+        for(int j =1; j<=rows - i;j++)
+        {
+            cout<<" ";
+        }
+        //stars
+        for(int c =1; c<=(2*i-1);c++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
