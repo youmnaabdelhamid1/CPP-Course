c7552311e216957b8d17e213138b1a8a5d779b8f
Youmna Elgzery <130307273+youmnaabdelhamid1@users.noreply.github.com>
2025-02-08 16:30:06 +0200

    Task1 H7.5 main.cpp

---
+++ Task1/H7.5 main.cpp
@@ -0,0 +1,32 @@
+//Task1.H7.5
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num, result;
+    cin>> result;
+
+    cin>> num ;
+    if(result<num) result = num;
+    cin>> num ;
+    if(result<num) result = num;
+    cin>> num ;
+    if(result<num) result = num;
+    cin>> num ;
+    if(result<num) result = num;
+    cin>> num ;
+    if(result<num) result = num;
+    cin>> num ;
+    if(result<num) result = num;
+    cin>> num ;
+    if(result<num) result = num;
+    cin>> num ;
+    if(result<num) result = num;
+    cin>> num ;
+    if(result<num) result = num;
+
+    cout<< result;
+    return 0;
+}
