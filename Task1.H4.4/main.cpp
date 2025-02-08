e4ea85617178f5fc584980a11519d7430b4a1d25
Youmna Elgzery <130307273+youmnaabdelhamid1@users.noreply.github.com>
2025-02-08 13:32:52 +0200

    Task1.H4.4 main.cpp

---
+++ Task1.H4.4/main.cpp
@@ -0,0 +1,19 @@
+//Task1.H4.4
+#include<iostream>
+using namespace std;
+
+int main() {
+	int num1, num2, num3;
+
+	num1 = 0, num2 = 1, num3 = num1 + num2, cout <<num3<<"\n";      //1
+	num1 = num2, num2 = num3, num3 = num1 + num2, cout <<num3<<"\n";//2
+	num1 = num2, num2 = num3, num3 = num1 + num2, cout <<num3<<"\n";//3
+	num1 = num2, num2 = num3, num3 = num1 + num2, cout <<num3<<"\n";//5
+	num1 = num2, num2 = num3, num3 = num1 + num2, cout <<num3<<"\n";//8
+	num1 = num2, num2 = num3, num3 = num1 + num2, cout <<num3<<"\n";//13
+	num1 = num2, num2 = num3, num3 = num1 + num2, cout <<num3<<"\n";//21
+	num1 = num2, num2 = num3, num3 = num1 + num2, cout <<num3<<"\n";//34
+	num1 = num2, num2 = num3, num3 = num1 + num2, cout <<num3<<"\n";//55
+
+	return 0;
+}
