e2a30dfb3319dbe9f6dd0d839b0df7492f161043
Youmna Elgzery <130307273+youmnaabdelhamid1@users.noreply.github.com>
2025-02-08 16:20:38 +0200

    Task1 H6.6 main.cpp

---
+++ Task1/H6.6 main.cpp
@@ -0,0 +1,15 @@
+//Task1.H6.6
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    double a,b;
+    cin>> a >> b ;
+    int div1= a/b;
+    double div2=a/b;
+    cout<< (div2 - div1)*b <<endl;
+    return 0;
+}
