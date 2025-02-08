56b7f2f5559f945d535136f6cbb5ed95ca60dae8
Youmna Elgzery <130307273+youmnaabdelhamid1@users.noreply.github.com>
2025-02-08 13:15:29 +0200

    Task1.H2.3 main.cpp

---
+++ Task1.H2.3/main.cpp
@@ -0,0 +1,25 @@
+// مثلث قائم
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n =5;
+    for(int i =1; i<=n ; i++)
+    {
+        for(int j =1 ; j<=i ; j++)
+        {
+            cout<< "*";
+        }
+        cout<<endl;
+    }
+
+   // cout << "*" << endl;
+   // cout << "**" << endl;
+   // cout << "***" << endl;
+   // cout << "****" << endl;
+   // cout << "*****" << endl;
+
+    return 0;
+}
