65e36d0b24af43f7a5aef5bb9c34ff106d616c2b
Youmna Elgzery <130307273+youmnaabdelhamid1@users.noreply.github.com>
2025-02-10 19:45:28 +0200

    Update H4.9 main.cpp

--- Task1/H4.9 main.cpp
+++ Task1/H4.9 main.cpp
@@ -14,5 +14,22 @@ int main()
         sum += i;
     }
     cout<<sum<<endl;
+
+    //another solution
+    #include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter n: ";
+    cin >> n;
+
+    
+    int sum = n * (n + 1) / 2;
+
+    cout << "Sum: " << sum << endl;
+    return 0;
+}
+
     return 0;
 }
