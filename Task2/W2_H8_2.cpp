dad4356fd0937757cb618bb9a04c7227930a7f7c
Youmna Elgzery <130307273+youmnaabdelhamid1@users.noreply.github.com>
2025-02-28 12:13:24 +0200

    W2_H8_2.cpp

---
+++ Task2/W2_H8_2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num;
+    string symbol;
+    cin>> num >> symbol;
+    string result;
+    for(int i= 0; i<num; i++)
+    {
+        cout<<  symbol;
+    }
+    return 0;
+}
