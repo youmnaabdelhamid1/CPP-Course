7c3d32ba5ddcea601b9f142a92b0a6b4081ce157
Youmna Elgzery <130307273+youmnaabdelhamid1@users.noreply.github.com>
2025-02-28 12:12:51 +0200

    W2_H8_1.cpp

---
+++ Task2/W2_H8_1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>;
+
+using namespace std;
+
+int main()
+{
+    int startNum , endNum;
+    cin >> startNum >> endNum ;
+    cout <<endl;
+    int num =0;
+
+    for(int i = startNum; i<=endNum ;i++)
+    {
+        cout<< i << endl;
+    }
+    return 0;
+}
